65d33abfa56431f7d22257efa55a84b61db1cad7
rayalva407 <rayalva407@gmail.com>
2020-05-13 10:25:23 -0600

    added show action

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -3,4 +3,13 @@ class PoemsController < ApplicationController
     @poems = Poem.all
     erb :"poems/index"
   end
+
+  get '/poems/:id' do
+    @poem = Poem.find_by_id(params[:id])
+    if @poem
+      erb :"poems/show"
+    else
+      redirect to '/poems'
+    end
+  end
 end
