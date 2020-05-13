4f202cb245e0cfe0a05fa850fe7aca3d2ce0d0b3
rayalva407 <rayalva407@gmail.com>
2020-05-13 10:46:48 -0600

    adds poems/new page

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -4,6 +4,15 @@ class PoemsController < ApplicationController
     erb :"poems/index"
   end
 
+  get '/poems/new' do
+    @poems = Poem.all
+    erb :"poems/new"
+  end
+
+  post '/poems' do
+
+  end
+
   get '/poems/:id' do
     @poem = Poem.find_by_id(params[:id])
     if @poem
