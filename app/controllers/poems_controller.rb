b9ab10edfae0abbc875aac29e0a44f329d0a8b8b
rayalva407 <rayalva407@gmail.com>
2020-05-14 22:16:36 -0600

    checks for logged_in? in poems_controller

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,57 +1,81 @@
 class PoemsController < ApplicationController
 
   get '/poems' do
-    binding.pry
-    @poems = Poem.all
-    erb :"poems/index"
+    if logged_in?
+      @poems = current_user.poems
+      erb :"poems/index"
+    else
+      redirect '/login'
+    end
   end
 
   get '/poems/new' do
-    @users = User.all
-    erb :"poems/new"
+    if logged_in?
+      erb :"poems/new"
+    else
+      redirect '/login'
+    end
   end
 
   post '/poems' do
-    #binding.pry
-    user = User.find_by_id(params[:user_id])
-    poem = user.poems.build(params)
-    if poem.save
-      redirect to '/poems/:id'
+    if logged_in?
+      poem = current_user.poems.build(params)
+      if poem.save
+        redirect to '/poems/:id'
+      else
+        redirect to '/poems/new'
+      end
     else
-      redirect to '/poems/new'
+      redirect '/login'
     end
   end
 
   get '/poems/:id/edit' do
-    @users = User.all
-    @poem = Poem.find_by_id(params[:id])
-    erb :"poems/edit"
+    if logged_in?
+      @poem = current_user.poems.find_by_id(params[:id])
+      if @poem
+      erb :"poems/edit"
+      else
+        redirect to '/poems'
+      end
+    else
+      redirect to '/login'
+    end
   end
 
   patch '/poems/:id' do
-    binding.pry
-    @poem = Poem.find_by_id(params[:id])
-    if @poem.update(title: params[:title], content: params[:content])
-      redirect to "poems/#{@poem.id}"
+    if logged_in?
+      @poem = current_user.poems.find_by_id(params[:id])
+      if @poem.update(title: params[:title], content: params[:content])
+        redirect to "poems/#{@poem.id}"
+      else
+        redirect to "poems/#{@poem.id}/edit"
+      end
     else
-      redirect to "poems/#{@poem.id}/edit"
+      redirect to '/login'
     end
   end
 
   get '/poems/:id' do
-    @poem = Poem.find_by_id(params[:id])
-    if @poem
-      erb :"poems/show"
+    if logged_in?
+      @poem = current_user.poems.find_by_id(params[:id])
+      if @poem
+        erb :"poems/show"
+      else
+        redirect to '/poems'
+      end
     else
-      redirect to '/poems'
-    end
+      redirect to '/login'
   end
 
   delete '/poems/:id' do
-    @poem = Poem.find_by_id(params[:id])
-    if @poem
-      @poem.delete
-    end
-    redirect to '/poems'
+    if logged_in?
+      @poem = current_user.poems.find_by_id(params[:id])
+      if @poem
+        @poem.delete
+      end
+      redirect to '/poems'
+    else
+      redirect to '/login'
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect "/posts"
+      redirect "/poems"
     else
       redirect to '/login'
     end
