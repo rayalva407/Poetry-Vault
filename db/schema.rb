28aca2b1118e7960b088e9a912127d779bee1237
rayalva407 <rayalva407@gmail.com>
2020-05-14 20:57:45 -0600

    adds password migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200513154540) do
+ActiveRecord::Schema.define(version: 20200515025431) do
 
   create_table "poems", force: :cascade do |t|
     t.string  "title"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20200513154540) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
