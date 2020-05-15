622c590d9eba4f418509b1ccb300e2e093539ddb
rayalva407 <rayalva407@gmail.com>
2020-05-14 23:53:11 -0600

    Modified welcome.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
-  
+
+  get '/signup' do
+    erb :'/users/new'
+  end
+
+  post '/signup' do
+    binding.pry
+    user = User.new(params)
+    if user.save
+      redirect to '/poems'
+    else
+      redirect to '/signup'
+    end
+  end
 end
--- app/models/User.rb
+++ app/models/User.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
   has_many :poems
   has_secure_password
+  validates :username, :email, :password, presence: true
 end
